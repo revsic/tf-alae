feb956f7a361c223a9a70fd5e9554015f57f6824
revsic <revsic99@gmail.com>
2020-05-05 18:32:02 +0900

    trainer: Add image and mean test metrics

--- trainer.py
+++ trainer.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import tensorflow as tf
 import tqdm
 
@@ -19,17 +20,52 @@ class Trainer:
         for _ in tqdm.tqdm(range(epochs)):
             for datum in trainset:
                 step += 1
+                datum = tf.reshape(datum, (datum.shape[0], -1))
                 losses = model.trainstep(datum)
                 self.write_summary(losses, step)
-            
+
+            _, flat = model(datum)
+            self.write_image(flat, step)
+
+            metrics = []
             for datum in testset:
-                self.write_summary(model.losses(datum), step, train=False)
+                datum = tf.reshape(datum, (datum.shape[0], -1))
+                metrics.append(model.losses(datum))
+
+            self.write_summary(self.mean_metric(metrics), step, train=False)
+
+            _, flat = model(datum)
+            self.write_image(flat, step, train=False)
 
     def write_summary(self, metrics, step, train=True):
         summary = self.train_summary if train else self.test_summary
         with summary.as_default():
             for key, value in metrics.items():
                 tf.summary.scalar(key, value, step=step)
+
+    def write_image(self, flat, step, train=True, name='image'):
+        idx = np.random.randint(flat.shape[0])
+        summary = self.train_summary if train else self.test_summary
+        with summary.as_default():
+            tf.summary.image(
+                name,
+                tf.reshape(flat[idx:idx + 1], (1, 28, 28, 1)),
+                step=step)
+    
+    def mean_metric(self, metrics):
+        size = 0
+        avgs = {}
+        for metric in metrics:
+            size += 1
+            for key, value in metric.items():
+                if key not in avgs:
+                    avgs[key] = 0
+                avgs[key] += value
+        
+        for key in avgs.keys():
+            avgs[key] /= size
+
+        return avgs
 
 
 if __name__ == '__main__':
