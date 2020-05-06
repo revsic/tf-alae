45332c1c63426e02c018d10c2f50ed379948b2dd
revsic <revsic99@gmail.com>
2020-05-06 22:52:32 +0900

    trainer: Add annotation

--- trainer.py
+++ trainer.py
@@ -9,6 +9,8 @@ from mlpalae import MlpAlae
 
 
 class Trainer:
+    """ALAE trainer, now just for MNIST and MLP-ALAE.
+    """
     def __init__(self, summary_path, ckpt_path):
         train_path = os.path.join(summary_path, 'train')
         test_path = os.path.join(summary_path, 'test')
@@ -17,8 +19,16 @@ class Trainer:
         self.ckpt_path = ckpt_path
 
     def train(self, model, epochs, trainset, testset):
+        """Train ALAE model with given datasets.
+        Args:
+            model: ALAE, tf.keras.Model, ALAE model.
+            epochs: int, the number of the epochs.
+            trainset: tf.data.Dataset, training dataset.
+            testset: tf.data.Dataset, test dataset.
+        """
         step = 0
         for _ in tqdm.tqdm(range(epochs)):
+            # training phase
             for datum in trainset:
                 step += 1
                 datum = tf.reshape(datum, (datum.shape[0], -1))
@@ -28,6 +38,7 @@ class Trainer:
             _, flat = model(datum)
             self.write_image(flat, step)
 
+            # test phase
             metrics = []
             for datum in testset:
                 datum = tf.reshape(datum, (datum.shape[0], -1))
@@ -38,25 +49,48 @@ class Trainer:
             _, flat = model(datum)
             self.write_image(flat, step, train=False)
 
+            # write checkpoint
             model.save_weights(self.ckpt_path)
 
     def write_summary(self, metrics, step, train=True):
+        """Write tensorboard summary.
+        Args:
+            metrics: Dict[str, np.array], metrics.
+            step: int, current step.
+            train: bool, whether in training phase or test phase.
+        """
         summary = self.train_summary if train else self.test_summary
         with summary.as_default():
             for key, value in metrics.items():
                 tf.summary.scalar(key, value, step=step)
 
     def write_image(self, flat, step, train=True, name='image'):
+        """Write image to the tensorboard summary.
+        Args:
+            flat: tf.Tensor, [B, 784 + alpha], autoencoded image.
+            step: int, current step.
+            train: bool, whether in training phase or test phase.
+            name: str, image name for tensorboard.
+        """
+        # random index
         idx = np.random.randint(flat.shape[0])
+        # denormalize image
         image = tf.clip_by_value(flat[idx], -1, 1)
         summary = self.train_summary if train else self.test_summary
         with summary.as_default():
+            # write tensorboard summary
             tf.summary.image(
                 name,
                 tf.reshape(image[:784], (1, 28, 28, 1)),
                 step=step)
     
     def mean_metric(self, metrics):
+        """Compute mean of the metrics.
+        Args:
+            List[Dict[str, np.array]], metrics.
+        Returns:
+            Dict[str, np.array], mean metrics.
+        """
         size = 0
         avgs = {}
         for metric in metrics:
@@ -73,6 +107,7 @@ class Trainer:
 
 
 if __name__ == '__main__':
+    # set random seed
     tf.random.set_seed(1234)
     np.random.seed(1234)
 
