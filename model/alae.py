d974a27385361f04cd0a17357b8e387e6a5a9b2b
revsic <revsic99@gmail.com>
2020-05-11 00:39:51 +0900

    alae: Move loss and trainstep to upper class

--- model/alae.py
+++ model/alae.py
@@ -8,41 +8,178 @@ class ALAE(tf.keras.Model):
     def __init__(self):
         super(ALAE, self).__init__()
 
-    def encoder(self, *args, **kwargs):
-        """Encode data to fixed length latent vector.
-        Returns:
-            tf.Tensor, [B, ...], latent vectors.
+    def prepare(self, z_dim, gamma, learning_rate, beta1, beta2):
+        """Prepare for training and inference.
         """
-        raise NotImplementedError('ALAE.encoder is not implemented')
+        self.z_dim = z_dim
+        self.gamma = gamma
 
-    def generator(self, *args, **kwargs):
-        """Generate data from encoded latent.
+        self.map = self.mapper()
+        self.gen = self.generator()
+        self.enc = self.encoder()
+        self.dec = self.discriminator()
+
+        self.fakepass = tf.keras.Sequential([
+            self.map, self.gen, self.enc, self.dec])
+        self.realpass = tf.keras.Sequential([self.enc, self.dec])
+        self.latentpass = tf.keras.Sequential([self.map, self.gen, self.enc])
+
+        self.ed_var = self.enc.trainable_variables + self.dec.trainable_variables
+        self.fg_var = self.map.trainable_variables + self.gen.trainable_variables
+        self.eg_var = self.enc.trainable_variables + self.gen.trainable_variables
+
+        self.ed_opt = tf.keras.optimizers.Adam(learning_rate, beta1, beta2)
+        self.fg_opt = tf.keras.optimizers.Adam(learning_rate, beta1, beta2)
+        self.eg_opt = tf.keras.optimizers.Adam(learning_rate, beta1, beta2)
+
+    def encode(self, *args, **kwargs):
+        """Encode the input tensors to latent vectors.
+        Args:
+            _: tf.Tensor, [B, ...], input tensors.
         Returns:
-            tf.Tensor, [B, ...], input tensors.
+            _: tf.Tensor, [B, latent_dim], latent vectors.
         """
-        raise NotImplementedError('ALAE.generator is not implemented')
+        return self.enc(*args, **kwargs)
+    
+    def generate(self, *args, **kwargs):
+        """Generate output tensors from latent vectors.
+        Args:
+            _: tf.Tensor, [B, latent_dim], latent vectors.
+        Returns:
+            _: tf.Tensor, [B, ...], output tensors.
+        """
+        return self.gen(*args, **kwargs)
 
     def call(self, x):
         """Generate the latent vectors and autoencode the inputs.
         Args:
             x: tf.Tensor, [B, ...], input tensors.
         Returns:
-            latent: tf.Tensor, [B, ...], latent vectors.
+            latent: tf.Tensor, [B, latent_dim], latent vectors.
             _: tf.Tensor, [B, ...], autoencoded tensors.
         """
-        latent = self.encoder(x)
-        return latent, self.generator(latent)
-    
-    def losses(self, x):
-        """Return loss values, discrimnator, generator, latent loss.
+        latent = self.encode(x)
+        return latent, self.generate(latent)
+
+    @tf.function
+    def _disc_loss(self, z, x):
+        """Compute discriminator loss.
+        Args:
+            z: tf.Tensor, [B, z_dim], latent prior.
+            x: tf.Tensor, [B, ...], output tensors.
         Returns:
-            Dict[str, np.ndarray], loss values.
+            tf.Tensor, [], loss value.
         """
-        raise NotImplementedError('ALAE.metrics is not implemented')
+        with tf.GradientTape() as tape:
+            fakeloss = tf.reduce_mean(tf.math.softplus(self.fakepass(z)))
+            realloss = tf.reduce_mean(tf.math.softplus(-self.realpass(x)))
+
+        # gradient regularizer
+        grad = tape.gradient(realloss, self.ed_var)
+        gradreg = self.gamma / 2 * tf.reduce_mean([
+            tf.reduce_mean(tf.square(g)) for g in grad])
+
+        return fakeloss + realloss + gradreg
+
+    @tf.function
+    def _gen_loss(self, z, _=None):
+        """Compute generator loss.
+        Args:
+            z: tf.Tensor, [B, z_dim], latent prior.
+            _: unused, placeholder.
+        Returns:
+            tf.Tensor, [], generator loss value.
+        """
+        return tf.reduce_mean(tf.math.softplus(-self.fakepass(z)))
+
+    @tf.function
+    def _latent_loss(self, z, _=None):
+        """Compute latent loss.
+        Args:
+            z: tf.Tensor, [B, z_dim], latent prior.
+            _: unused, placeholder.
+        Returns:
+            tf.Tensor, [], latent loss value.
+        """
+        latent = self.f(z)
+        recovered = self.latentpass(z)
+        return tf.reduce_mean(tf.square(latent - recovered))
+
+    def losses(self, x):
+        """Loss values for tensorboard summary.
+        Args:
+            x: tf.Tensor, [B, ...], output samples.
+        Returns:
+            Dict[str, np.array], loss values.
+        """
+        bsize = x.shape[0]
+        z = tf.random.normal((bsize, self.z_dim), 0, 1)
+        return {
+            'disc': self._disc_loss(z, x).numpy(),
+            'gen': self._gen_loss(z).numpy(),
+            'latent': self._latent_loss(z).numpy(),
+        }
+
+    def _update(self, x, loss_fn, var, opt):
+        """Update weights with gradient and optimizer.
+        Args:
+            x: tf.Tensor, [B, ...], output samples.
+            loss_fn: Callable[[tf.Tensor, tf.Tensor], tf.Tensor],
+                loss function.
+            var: List[tf.Tensor], trainable variables.
+            opt: tf.keras.optimizers.Optimizer, keras optimizer.
+        Returns:
+            z: np.array, [B, z_dim], sampled latent prior.
+            loss: np.array, [], loss value.
+        """
+        z = tf.random.normal((x.shape[0], self.z_dim), 0, 1)
+        with tf.GradientTape() as tape:
+            loss = loss_fn(z, x)
+        
+        grad = tape.gradient(loss, var)
+        opt.apply_gradients(zip(grad, var))
+        return z.numpy(), loss.numpy()
 
     def trainstep(self, x):
-        """Optimize the model with given x.
+        """Optimize ALAE objective.
+        Args:
+            x: tf.Tensor, [B, ...], output samples.
         Returns:
-            Dict[str, np.ndarray], loss values.
+            Dict[str, np.array], loss values.
         """
-        raise NotImplementedError('ALAE.trainstep is not implemented')
+        _, dloss = self._update(x, self._disc_loss, self.ed_var, self.ed_opt)
+        _, gloss = self._update(x, self._gen_loss, self.fg_var, self.fg_opt)
+        _, lloss = self._update(x, self._latent_loss, self.eg_var, self.eg_opt)
+        return {
+            'disc': dloss,
+            'gen': gloss,
+            'latent': lloss,
+        }
+
+    def mapper(self, *args, **kwargs):
+        """Model for mpping latent from prior.
+        Returns:
+            tf.keras.Model: map prior to latent.
+        """
+        raise NotImplementedError('ALAE.mapper is not implemented')
+
+    def generator(self, *args, **kwargs):
+        """Model for generating data from encoded latent.
+        Returns:
+            tf.keras.Model: generate sample from encoded latent.
+        """
+        raise NotImplementedError('ALAE.generator is not implemented')
+
+    def encoder(self, *args, **kwargs):
+        """Model for encoding data to fixed length latent vector.
+        Returns:
+            tf.keras.Model: encode data to fixed length latent vector.
+        """
+        raise NotImplementedError('ALAE.encoder is not implemented')
+
+    def discriminator(self, *args, **kwargs):
+        """Model for discriminating real sample from fake one.
+        Returns:
+            tf.keras.Model: discriminate real sample from fake one.
+        """
+        raise NotImplementedError('ALAE.discriminator is not implemented')
--- model/mlpalae.py
+++ model/mlpalae.py
@@ -5,18 +5,6 @@ from ..alae import ALAE
 
 class MlpAlae(ALAE):
     """ALAE with multi-layer perceptron architecture.
-    Attributes:
-        z_dim: int, dimension of the latent prior.
-        latent_dim: int, dimension of the latent vector.
-        output_dim: int, dimension of the output vector.
-        gamma: int, coefficient for gradient regularization term.
-        f, g, e, d: tf.keras.Model, ALAE component,
-            latent-map, generator, encoder, discriminator.
-        fakepass, realpass, latentpass: tf.keras.Model,
-            additional component for computing objectives.
-        ed_var, fg_var, eg_var: List[tf.Tensor], trainable variables.
-        ed_opt, fg_opt, eg_opt: tf.keras.optimizers.Optimizer,
-            optimizers for training step.
     """
     def __init__(self, settings=None):
         """Constructor.
@@ -32,150 +20,52 @@ class MlpAlae(ALAE):
         self.latent_dim = settings['latent_dim']
         self.output_dim = settings['output_dim']
 
-        self.gamma = settings['gamma']
+        self.prepare(self.z_dim,
+                     settings['gamma'],
+                     settings['lr'],
+                     settings['beta1'],
+                     settings['beta2'])
 
-        def seq(dims, input_dim, activation=tf.nn.relu):
-            layer = tf.keras.Sequential([
-                tf.keras.layers.Dense(dim, activation=activation)
-                for dim in dims[:-1]])
-            layer.add(
-                tf.keras.layers.Dense(dims[-1]))
-            layer.build((None, input_dim))
-            return layer
+    def _seq(self, dims, input_dim, activation=tf.nn.relu):
+        layer = tf.keras.Sequential([
+            tf.keras.layers.Dense(dim, activation=activation)
+            for dim in dims[:-1]])
+        layer.add(
+            tf.keras.layers.Dense(dims[-1]))
+        layer.build((None, input_dim))
+        return layer
 
-        self.f = seq(settings['f'], self.z_dim)
-        self.g = seq(settings['g'], self.latent_dim)
-        self.e = seq(settings['e'], self.output_dim)
-        self.d = seq(settings['d'], self.latent_dim)
-
-        self.fakepass = tf.keras.Sequential([
-            self.f, self.g, self.e, self.d])
-        self.realpass = tf.keras.Sequential([self.e, self.d])
-        self.latentpass = tf.keras.Sequential([self.f, self.g, self.e])
-
-        self.ed_var = self.e.trainable_variables + self.d.trainable_variables
-        self.fg_var = self.f.trainable_variables + self.g.trainable_variables
-        self.eg_var = self.e.trainable_variables + self.g.trainable_variables
-
-        self.ed_opt = tf.keras.optimizers.Adam(
-            settings['lr'], settings['beta1'], settings['beta2'])
-        self.fg_opt = tf.keras.optimizers.Adam(
-            settings['lr'], settings['beta1'], settings['beta2'])
-        self.eg_opt = tf.keras.optimizers.Adam(
-            settings['lr'], settings['beta1'], settings['beta2'])
-
-    def encoder(self, *args, **kwargs):
-        """Encode the input tensors to latent vectors.
-        Args:
-            _: tf.Tensor, [B, output_dim], input tensors.
+    def mapper(self, *args, **kwargs):
+        """Model for mapping latent from prior.
         Returns:
-            _: tf.Tensor, [B, latent_dim], latent vectors.
+            tf.keras.Model: tf.Tensor[B, z_dim] -> tf.Tensor[B, latent_dim],
+                map prior to latent.
         """
-        return self.e(*args, **kwargs)
+        return self._seq(self.settings['f'], self.z_dim)
 
     def generator(self, *args, **kwargs):
-        """Generate output tensors from latent vectors.
-        Args:
-            _: tf.Tensor, [B, latent_dim], latent vectors.
+        """Model for generating data from encoded latent.
         Returns:
-            _: tf.Tensor, [B, output_dim], output tensors.
+            tf.keras.Model: tf.Tensor[B, latent_dim] -> tf.Tensor[B, output_dim],
+                generate sample from encoded latent.
         """
-        return self.g(*args, **kwargs)
+        raise self._seq(self.settings['g'], self.latent_dim)
 
-    @tf.function
-    def _disc_loss(self, z, x):
-        """Compute discriminator loss.
-        Args:
-            z: tf.Tensor, [B, z_dim], latent prior.
-            x: tf.Tensor, [B, output_dim], output tensors.
+    def encoder(self, *args, **kwargs):
+        """Model for encoding data to fixed length latent vector.
         Returns:
-            tf.Tensor, [], loss value.
+            tf.keras.Model: tf.Tensor[B, output_dim] -> tf.Tensor[B, latent_dim]
+                encode data to fixed length latent vector.
         """
-        with tf.GradientTape() as tape:
-            fakeloss = tf.reduce_mean(tf.math.softplus(self.fakepass(z)))
-            realloss = tf.reduce_mean(tf.math.softplus(-self.realpass(x)))
+        return self._seq(self.settings['e'], self.output_dim)
 
-        # gradient regularizer
-        grad = tape.gradient(realloss, self.ed_var)
-        gradreg = self.gamma / 2 * tf.reduce_mean([
-            tf.reduce_mean(tf.square(g)) for g in grad])
-
-        return fakeloss + realloss + gradreg
-
-    @tf.function
-    def _gen_loss(self, z, _=None):
-        """Compute generator loss.
-        Args:
-            z: tf.Tensor, [B, z_dim], latent prior.
-            _: unused, placeholder.
+    def discriminator(self, *args, **kwargs):
+        """Model for discriminating real sample from fake one.
         Returns:
-            tf.Tensor, [], generator loss value.
+            tf.keras.Model: tf.Tensor[B, latent_dim] -> tf.Tensor[B, 1]
+                discriminate real sample from fake one.
         """
-        return tf.reduce_mean(tf.math.softplus(-self.fakepass(z)))
-
-    @tf.function
-    def _latent_loss(self, z, _=None):
-        """Compute latent loss.
-        Args:
-            z: tf.Tensor, [B, z_dim], latent prior.
-            _: unused, placeholder.
-        Returns:
-            tf.Tensor, [], latent loss value.
-        """
-        latent = self.f(z)
-        recovered = self.latentpass(z)
-        return tf.reduce_mean(tf.square(latent - recovered))
-
-    def losses(self, x):
-        """Loss values for tensorboard summary.
-        Args:
-            x: tf.Tensor, [B, output_dim], output samples.
-        Returns:
-            Dict[str, np.array], loss values.
-        """
-        bsize = x.shape[0]
-        z = tf.random.normal((bsize, self.z_dim), 0, 1)
-        return {
-            'disc': self._disc_loss(z, x).numpy(),
-            'gen': self._gen_loss(z).numpy(),
-            'latent': self._latent_loss(z).numpy(),
-        }
-
-    def _update(self, x, loss_fn, var, opt):
-        """Update weights with gradient and optimizer.
-        Args:
-            x: tf.Tensor, [B, output_dim], output samples.
-            loss_fn: Callable[[tf.Tensor, tf.Tensor], tf.Tensor],
-                loss function.
-            var: List[tf.Tensor], trainable variables.
-            opt: tf.keras.optimizers.Optimizer, keras optimizer.
-        Returns:
-            z: np.array, [B, z_dim], sampled latent prior.
-            loss: np.array, [], loss value.
-        """
-        z = tf.random.normal((x.shape[0], self.z_dim), 0, 1)
-        with tf.GradientTape() as tape:
-            loss = loss_fn(z, x)
-        
-        grad = tape.gradient(loss, var)
-        opt.apply_gradients(zip(grad, var))
-        return z.numpy(), loss.numpy()
-
-    def trainstep(self, x):
-        """Optimize ALAE objective.
-        Args:
-            x: tf.Tensor, [B, output_dim], output samples.
-        Returns:
-            Dict[str, np.array], loss values.
-        """
-        _, dloss = self._update(x, self._disc_loss, self.ed_var, self.ed_opt)
-        _, gloss = self._update(x, self._gen_loss, self.fg_var, self.fg_opt)
-        _, lloss = self._update(x, self._latent_loss, self.eg_var, self.eg_opt)
-        return {
-            'disc': dloss,
-            'gen': gloss,
-            'latent': lloss,
-        }
+        raise self._seq(self.settings['d'], self.latent_dim)
 
     @staticmethod
     def default_setting(z_dim=128, latent_dim=50, output_dim=784 + 10):
