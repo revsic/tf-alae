8dd1696061c0a421cb0ff39a4cf18ce327a37417
revsic <revsic99@gmail.com>
2020-05-11 01:23:35 +0900

    mnist mlp: Add argparse

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import os
 
 import numpy as np
@@ -8,26 +8,41 @@ from utils.trainer import Trainer
 from datasets.mnist import MNIST
 from models.mlpalae import MlpAlae
 
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('option', type=str, default='train')
+PARSER.add_argument('--name', default='baseline')
+PARSER.add_argument('--summarydir', default='./summary')
+PARSER.add_argument('--ckptdir', default='./ckpt')
+PARSER.add_argument('--epochs', default=50, type=int)
+PARSER.add_argument('--seed', default=1234, type=int)
 
-def main(_):
-    # set random seed
-    tf.random.set_seed(1234)
-    np.random.seed(1234)
 
+def train(args):
     mnist = MNIST()
     mlpalae = MlpAlae()
 
-    modelname = 'baseline'
-    if not os.path.exists('./summary/' + modelname):
-        os.mkdir('./summary/' + modelname)
+    modelname = args.name
+    summary_path = os.path.join(args.summarydir, modelname)
+    if not os.path.exists(summary_path):
+        os.mkdir(summary_path)
     
-    if not os.path.exists('./ckpt'):
-        os.mkdir('./ckpt')
+    ckpt_path = os.path.join(args.ckptdir, modelname)
+    if not os.path.exists(args.ckptdir):
+        os.mkdir(args.ckptdir)
 
-    trainer = Trainer('./summary/' + modelname, './ckpt/' + modelname)
-    trainer.train(mlpalae, 50, mnist.cdatasets(), mnist.cdatasets(train=False))
+    trainer = Trainer(summary_path, ckpt_path)
+    trainer.train(mlpalae, args.epochs, mnist.cdatasets(), mnist.cdatasets(train=False))
     return 0
 
 
+def main(args):
+    # set random seed
+    tf.random.set_seed(args.seed)
+    np.random.seed(args.seed)
+
+    if args.option == 'train':
+        train(args)
+
+
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main(PARSER.parse_args())
