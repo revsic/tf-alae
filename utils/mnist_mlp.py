77ebb082ed27f394e7a8e170b2a8b7e019215e28
revsic <revsic99@gmail.com>
2020-05-11 00:47:14 +0900

    mnist_mlp: Split from trainer

---
+++ utils/mnist_mlp.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+import numpy as np
+import tensorflow as tf
+
+from ..datasets.mnist import MNIST
+from ..models.mlpalae import MlpAlae
+
+
+def main(_):
+    # set random seed
+    tf.random.set_seed(1234)
+    np.random.seed(1234)
+
+    mnist = MNIST()
+    mlpalae = MlpAlae()
+
+    modelname = 'lat50_gam10_sepopt_seed_beta0_epoch50'
+    if not os.path.exists('./summary/' + modelname):
+        os.mkdir('./summary/' + modelname)
+    
+    if not os.path.exists('./ckpt'):
+        os.mkdir('./ckpt')
+
+    trainer = Trainer('./summary/' + modelname, './ckpt/' + modelname)
+    trainer.train(mlpalae, 50, mnist.cdatasets(), mnist.cdatasets(train=False))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
--- utils/trainer.py
+++ utils/trainer.py
@@ -104,22 +104,3 @@ class Trainer:
             avgs[key] /= size
 
         return avgs
-
-
-if __name__ == '__main__':
-    # set random seed
-    tf.random.set_seed(1234)
-    np.random.seed(1234)
-
-    mnist = MNIST()
-    mlpalae = MlpAlae()
-
-    modelname = 'lat50_gam10_sepopt_seed_beta0_epoch50'
-    if not os.path.exists('./summary/' + modelname):
-        os.mkdir('./summary/' + modelname)
-    
-    if not os.path.exists('./ckpt'):
-        os.mkdir('./ckpt')
-
-    trainer = Trainer('./summary/' + modelname, './ckpt/' + modelname)
-    trainer.train(mlpalae, 50, mnist.cdatasets(), mnist.cdatasets(train=False))
