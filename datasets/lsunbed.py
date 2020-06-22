e1c35ea1095349e8f489807d71321b20abb14727
revsic <revsic99@gmail.com>
2020-06-23 03:07:56 +0900

    lsunbed: Add annotation

--- datasets/lsunbed.py
+++ datasets/lsunbed.py
@@ -5,11 +5,20 @@ import tensorflow as tf
 
 
 class LsunBed:
+    """LSUN Bed dataset wrapper.
+    Attributes:
+        path: str, path of the dataset.
+        size: int, image size.
+    """
     def __init__(self, path, size=256):
         self.path = path
         self.size = size
 
     def reader(self):
+        """Generator of reading images and normalizing for training.
+        Yields:
+            np.ndarray, [size, size, 3], normalized image in range (-1, 1).
+        """
         env = lmdb.open(self.path, map_size=1099511627776,
                         max_readers=100, readonly=True)
         with env.begin(write=False) as txn:
@@ -25,13 +34,13 @@ class LsunBed:
 
     @staticmethod
     def center_crop(img, h, w):
-        """
+        """Crop center of the image.
         Args:
-            img: np.ndarray, [H, W, C]
-            h: int, height.
-            w: int, width.
+            img: np.ndarray, [H, W, C], image array.
+            h: int, height of the output.
+            w: int, width of the output.
         Returns:
-            np.ndarray, [h, w, C]
+            np.ndarray, [h, w, C], cropped image.
         """
         height, width, _ = img.shape
         dh = (height - h) // 2
@@ -40,13 +49,23 @@ class LsunBed:
     
     @staticmethod
     def normalize(img):
+        """Normalize image in range(-1, 1).
+        Args:
+            img: np.ndarray, image array.
+        Returns:
+            np.ndarray, normalized images.
+        """
         return img / 127.5 - 1.
 
-    @staticmethod
-    def denormalize(img):
-        return (img + 1) * 127.5
-
-    def datasets(self, bsize=128, bufsiz=10000):
+    def datasets(self, bsize=16, bufsiz=10000):
+        """Image dataset.
+        Args:
+            bsize: int, batch size.
+            bufsiz: int, buffer size for shuffling.
+        Returns:
+            tf.data.Dataset, tensorflow dataset object,
+                Iterable[tf.Tensor=[B, size, size, 3]], iterable.
+        """
         return tf.data.Dataset.from_generator(self.reader, np.float32) \
             .shuffle(bufsiz) \
             .batch(bsize)
