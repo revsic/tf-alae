71ca546c3538212b384dcce6311a7859ce09b09f
revsic <revsic99@gmail.com>
2020-06-23 02:45:25 +0900

    lsunbed: Fix lsun bed reader

--- datasets/lsunbed.py
+++ datasets/lsunbed.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 
 
 class LsunBed:
-    def __init__(self, path):
+    def __init__(self, path, size=256):
         self.path = path
+        self.size = size
 
     def reader(self):
         env = lmdb.open(self.path, map_size=1099511627776,
@@ -14,9 +15,16 @@ class LsunBed:
         with env.begin(write=False) as txn:
             cursor = txn.cursor()
             for _, val in cursor:
-                yield cv2.imdecode(
+                img = cv2.imdecode(
                     np.fromstring(val, dtype=np.uint8), 1)
+                
+                height, width, _ = img.shape
+                dh = (height - self.size) // 2
+                dw = (width - self.size) // 2
+
+                yield img[dh:dh + self.size, dw:dw + self.size, :]
 
     def datasets(self, bsize, bufsiz):
-        return tf.data.Dataset.from_generator(
-            self.reader, np.uint8)
+        return tf.data.Dataset.from_generator(self.reader, np.uint8) \
+            .shuffle(bufsiz) \
+            .batch(bsize)
