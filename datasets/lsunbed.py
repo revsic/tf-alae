0a92191fab770597ee66f98161862f8edbbf200f
revsic <revsic99@gmail.com>
2020-06-23 02:53:43 +0900

    lsunbed: Add normalizing term

--- datasets/lsunbed.py
+++ datasets/lsunbed.py
@@ -15,16 +15,38 @@ class LsunBed:
         with env.begin(write=False) as txn:
             cursor = txn.cursor()
             for _, val in cursor:
+                # read image
                 img = cv2.imdecode(
                     np.fromstring(val, dtype=np.uint8), 1)
-                
-                height, width, _ = img.shape
-                dh = (height - self.size) // 2
-                dw = (width - self.size) // 2
+                # resize
+                img = LsunBed.center_crop(img, self.size, self.size)
+                # normalize
+                yield LsunBed.normalize(img)
 
-                yield img[dh:dh + self.size, dw:dw + self.size, :]
+    @staticmethod
+    def center_crop(img, h, w):
+        """
+        Args:
+            img: np.ndarray, [H, W, C]
+            h: int, height.
+            w: int, width.
+        Returns:
+            np.ndarray, [h, w, C]
+        """
+        height, width, _ = img.shape
+        dh = (height - h) // 2
+        dw = (width - w) // 2
+        return img[dh:dh + h, dw:dw + w, :]
+    
+    @staticmethod
+    def normalize(img):
+        return img / 127.5 - 1.
+
+    @staticmethod
+    def denormalize(img):
+        return (img + 1) * 127.5
 
     def datasets(self, bsize, bufsiz):
-        return tf.data.Dataset.from_generator(self.reader, np.uint8) \
+        return tf.data.Dataset.from_generator(self.reader, np.float32) \
             .shuffle(bufsiz) \
             .batch(bsize)
