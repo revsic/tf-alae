9303a4102234ad01a155078932e27167eecbf150
revsic <revsic99@gmail.com>
2020-06-24 16:51:47 +0900

    map latent: Add annotation

--- models/stylealae/maplatent.py
+++ models/stylealae/maplatent.py
@@ -2,7 +2,15 @@ import tensorflow as tf
 
 
 class LatentMap(tf.keras.Model):
+    """Generate style vector from prior.
+    """
     def __init__(self, num_layer, latent_dim, hidden_dim):
+        """Initializer.
+        Args:
+            num_layer: int, number of the layer.
+            latent_dim: int, size of the style vector.
+            hidden_dim: int, size of the hidden units.
+        """
         super(LatentMap, self).__init__()
         self.num_layer = num_layer
         self.latent_dim = latent_dim
@@ -17,10 +25,10 @@ class LatentMap(tf.keras.Model):
             tf.keras.layers.Dense(self.latent_dim))
 
     def call(self, z):
-        """
+        """Generate style vector.
         Args:
-            z: tf.Tensor, [B, z_dim]
+            z: tf.Tensor, [B, z_dim], style prior.
         Returns:
-            tf.Tensor, [B, latent_dim]
+            tf.Tensor, [B, latent_dim], style vector.
         """
         return self.blocks(z)
