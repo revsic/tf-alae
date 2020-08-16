6cff23f114b720b0ac072d34698d3e4f40de66ee
revsic <revsic99@gmail.com>
2020-08-16 20:27:29 +0900

    stylealae: Add preproc and fix builder

--- models/experimental/stylealae/stylealae.py
+++ models/experimental/stylealae/stylealae.py
@@ -28,6 +28,7 @@ class StyleAlae(ALAE):
         self.max_channels = self.settings['max_channels']
         self.out_channels = self.settings['out_channels']
 
+        self.level = 0
         self.img_size = 2 ** (self.num_layer + 1)
 
         self.prepare(self.latent_dim,
@@ -44,14 +45,57 @@ class StyleAlae(ALAE):
         Args:
             level: int, training level, in range [0, num_layer).
         """
-        self.gen.set_level(level)
-        self.enc.set_level(level)
+        self.level = level
+        self.gen.set_level(self.level)
+        self.enc.set_level(self.level)
 
         gen_var = self.gen.level_variables()
         enc_var = self.enc.level_variables()
         self.ed_var = enc_var + self.disc.trainable_variables
         self.fg_var = self.map.trainable_variables + gen_var
         self.eg_var = enc_var + gen_var
+
+    def preproc(self, x):
+        """Preprocess inputs to resize with respects to level.
+        Args:
+            x: tf.Tensor, [B, H, W, C], image tensor.
+        Returns:
+            tf.Tensor, [B, S, S, C], downsampled image
+                where S = 2 ** (level + 2).
+        """
+        size = 2 ** (self.level + 2)
+        x = tf.image.resize(x, [size, size])
+        return x
+
+    def encode(self, x):
+        """Encode the input tensors to latent vectors.
+        Args:
+            x: tf.Tensor, [B, H, W, C], image tensor.
+        Returns:
+            tf.Tensor, [B, latent_dim], latent_vectors
+        """
+        return self.enc(self.preproc(x))
+
+    @tf.function
+    def _rctor_loss(self, _, x):
+        styles = self.enc(x)
+        rctor = self.gen(styles)
+        # rctor = self.gen(self.enc(x))
+        return tf.reduce_mean(tf.abs(rctor - x))
+
+    def losses(self, x):
+        x = self.preproc(x)
+        # losses = super(StyleAlae, self).losses(x)
+        losses = {}
+        losses['rctor'] = self._rctor_loss(None, x)
+        return losses
+
+    def trainstep(self, x):
+        x = self.preproc(x)
+        # losses = super(StyleAlae, self).trainstep(x)
+        losses = {}
+        _, losses['rctor'] = self._update(x, self._rctor_loss, self.eg_var, self.rctor_opt)
+        return losses
 
     def mapper(self):
         """Model for mpping latent from prior.
@@ -73,7 +117,9 @@ class StyleAlae(ALAE):
                         max_channels=self.max_channels,
                         num_layer=self.num_layer,
                         out_channels=self.out_channels)
-        gen.build((None, self.latent_dim))
+        for i in range(self.num_layer, 0, -1):
+            gen.set_level(i - 1)
+            gen.build((None, self.latent_dim))
         return gen
 
     def encoder(self):
@@ -85,7 +131,12 @@ class StyleAlae(ALAE):
                       max_channels=self.max_channels,
                       num_layer=self.num_layer,
                       latent_dim=self.latent_dim)
-        enc.build((None, self.img_size, self.img_size, self.out_channels))
+        
+        img_size = self.img_size
+        for i in range(self.num_layer, 0, -1):
+            enc.set_level(i - 1)
+            enc.build((None, img_size, img_size, self.out_channels))
+            img_size //= 2
         return enc
 
     def discriminator(self):
@@ -98,25 +149,6 @@ class StyleAlae(ALAE):
                          hidden_dim=self.latent_dim)
         disc.build((None, self.latent_dim))
         return disc
-
-    @tf.function
-    def _rctor_loss(self, _, x):
-        rctor = self.gen(self.enc(x))
-        _, height, width, _ = rctor.shape
-        x = tf.image.resize(x, [height, width])
-        return tf.reduce_mean(tf.abs(rctor - x))
-
-    def losses(self, x):
-        # losses = super(StyleAlae, self).losses(x)
-        losses = {}
-        losses['rctor'] = self._rctor_loss(None, x)
-        return losses
-
-    def trainstep(self, x):
-        # losses = super(StyleAlae, self).trainstep(x)
-        losses = {}
-        _, losses['rctor'] = self._update(x, self._rctor_loss, self.eg_var, self.rctor_opt)
-        return losses
 
     @staticmethod
     def default_setting(imgsize=256):
